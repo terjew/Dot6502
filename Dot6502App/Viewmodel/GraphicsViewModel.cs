d867e4902894fb474989ec38a5c765905658fc81
Terje Wiesener <wiesener@gmail.com>
2020-11-22 23:53:07 +0100

    Added some graphics options

--- Dot6502App/Viewmodel/GraphicsViewModel.cs
+++ Dot6502App/Viewmodel/GraphicsViewModel.cs
@@ -12,12 +12,32 @@ namespace Dot6502App.Viewmodel
 {
     public enum GraphicsMode
     {
-        Palette16Color,
+        Palette16,
         RGB332
     }
 
     class GraphicsViewModel : BindableBase
     {
+        private int[] colormap = new []
+        {
+            0x000000,//Black
+            0xffffff,//White
+            0x880000,//Red
+            0xaaffee,//Cyan
+            0xcc44cc,//Purple
+            0x00cc55,//Green
+            0x0000aa,//Blue
+            0xeeee77,//Yellow
+            0xdd8855,//Orange
+            0x664400,//Brown
+            0xff7777,//Light red
+            0x333333,//Dark gray
+            0x777777,//Gray
+            0xaaff66,//Light green
+            0x0088ff,//Light blue
+            0xbbbbbb,//Light gray
+        };
+
         WriteableBitmap _bitmap;
         public WriteableBitmap Bitmap
         {
@@ -25,16 +45,57 @@ namespace Dot6502App.Viewmodel
             set => SetProperty(ref _bitmap, value);
         }
 
-        private int width;
-        private int height;
+        public List<int> ResolutionValues { get; } = new List<int>() { 32, 64, 96, 128 };
+        public List<GraphicsMode> ModeValues { get; } = new List<GraphicsMode>() { GraphicsMode.Palette16, GraphicsMode.RGB332 };
+        public List<BitmapScalingMode> ScalingModes { get; } = new List<BitmapScalingMode>() { BitmapScalingMode.HighQuality, BitmapScalingMode.NearestNeighbor };
+
         private GraphicsMode mode;
+        public GraphicsMode Mode
+        {
+            get => mode;
+            set => SetProperty(ref mode, value);
+        }
+
+        private BitmapScalingMode scalingMode;
+        public BitmapScalingMode ScalingMode
+        {
+            get => scalingMode;
+            set => SetProperty(ref scalingMode, value);
+        }
+
+        private int width;
+        public int Width
+        {
+            get => width;
+            set => SetProperty(ref width, value);
+        }
+
+        private int height;
+        public int Height
+        {
+            get => height;
+            set => SetProperty(ref height, value);
+        }
+
         public GraphicsViewModel(int width, int height, GraphicsMode mode)
         {
-            this.width = width;
-            this.height = height;
-            this.mode = mode;
+            Width = width;
+            Height = height;
+            Mode = mode;
 
-            _bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Pbgra32, null);
+            PropertyChanged += GraphicsViewModel_PropertyChanged;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Pbgra32, null);
+        }
+
+        private void GraphicsViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(Bitmap)) Initialize();
         }
 
         public void Update(byte[] memory, int offset)
@@ -63,8 +124,6 @@ namespace Dot6502App.Viewmodel
                                 srcPtr += 1;
                                 dstPtr += 4;
                             }
-                            //srcPtr += width;
-                            //dstPtr += _bitmap.BackBufferStride;
                         }
                     }
 
@@ -82,17 +141,20 @@ namespace Dot6502App.Viewmodel
 
         private int ToRgb(byte src)
         {
-            var r = src & 0b11100000;
-            var g = (src & 0b00011100) << 3;
-            var b = (src & 0b00000011) << 6;
+            if (mode == GraphicsMode.RGB332)
+            {
+                var r = src & 0b11100000;
+                var g = (src & 0b00011100) << 3;
+                var b = (src & 0b00000011) << 6;
 
-            // Compute the pixel's color.
-            int color_data = 255 << 24;
-            color_data |= r << 16; // R
-            color_data |= g << 8;   // G
-            color_data |= b << 0;   // B
+                int color_data = 255 << 24; //A
+                color_data |= r << 16; // R
+                color_data |= g << 8; // G
+                color_data |= b << 0; // B
 
-            return color_data;
+                return color_data;
+            }
+            return colormap[src & 0x0f];
         }
     }
 }
